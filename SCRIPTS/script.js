1814777cd5e46909bf3b4dfcac9530ce4f66bb61
Tomasz Pietrzak <tomaszpietrzak14@gmail.com>
2022-01-25 18:53:54 +0100

    finished work

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,14 +1,13 @@
 'use strict';
 
 require([
-    "esri/config",
     "esri/Map",
-    "dijit/form/Button",
     "esri/views/SceneView",
     "esri/layers/FeatureLayer",
-], function (esriConfig, Map, Button,SceneView, FeatureLayer) {
-
-    esriConfig.apiKey = "AAPKb3392f1605744b938328349d8bbcc26aYzDBpljmCG_A187wY3SjiBZyWhphttQP1yLgvIjXQAabtCHZh-Pmpg1DTRnq4eQ6"
+    "esri/layers/GraphicsLayer",
+    "esri/widgets/Legend",
+    "esri/rest/support/Query"
+], function (Map,SceneView, FeatureLayer, GraphicsLayer, Legend, Query) {
 
     const map = new Map({
         basemap: "topo-vector",
@@ -18,16 +17,115 @@ require([
     const view = new SceneView({
         container: "map3d", 
         map: map,
-        zoom: 4
+        zoom: 4,
+        center: [-103.7715 ,44.9672]
     });
 
+    // Warstwy
 
-    const fl = new FeatureLayer({
+    let gl = new GraphicsLayer();
+    let g2 = new GraphicsLayer();
+
+    const f1 = new FeatureLayer({
         url: "https://services.arcgis.com/ue9rwulIoeLEI9bj/ArcGIS/rest/services/Earthquakes/FeatureServer/0"
     });
 
+    const f2 = new FeatureLayer({
+        url: "https://services.arcgis.com/ue9rwulIoeLEI9bj/ArcGIS/rest/services/Earthquakes/FeatureServer/0"
+    });
+
+    map.add(f2)
+    map.add(gl);
+    map.add(g2);
+
     // Widgety
+
+    const legend = new Legend({
+        view: view
+    });
+
+    view.ui.add(legend, {position: "bottom-right"});
     
+    let query = f1.createQuery();
+
+    query.where = "MAGNITUDE > 4";
+    query.outFields = ['*'];
+    query.returnGeometry = true;
+
+    f1.queryFeatures(query)
+    .then(response => {
+        console.log(response);
+        getResults(response.features);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+    const getResults = (features) => {
+        let symbol = {
+            type: "simple-marker",
+            size: 15,
+            color: "red",
+            style: "triangle"
+        };
+        
+        features.map(elem => {
+            elem.symbol = symbol;
+        });
+
+        gl.addMany(features)
+    };
+
+    // Rendering
+
+    const simple = {
+        type: "simple",
+        symbol: {
+            type: "point-3d",
+            symbolLayers: [
+                {
+                    type: "object",
+                    resource: {
+                        primitive: "cylinder"
+                    },
+                    width:5000
+                },
+            ]
+        },
+        label: "Earthquake",
+        visualVariables: [
+            {
+                type: "color",
+                field: "MAGNITUDE",
+                stops: [
+                    {
+                        value: 0.5,
+                        color: "green"
+                    },
+                    {
+                        value: 4,
+                        color: "red"
+                    },
+                ]
+            },
+            {
+                type: "size",
+                field: "DEPTH",
+                stops: [
+                    {
+                        value: -3.39,
+                        size: 5000
+                    },
+                    {
+                        value: 30.97,
+                        size: 13000
+                    },
+                ]
+            }
+        ]
+    };
+
+    f2.renderer = simple;
+
 
 });
-
